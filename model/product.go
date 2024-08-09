2f267d0a1cfbe4a4d5afc922e8de91c49fc3eaae
Ilyas <bdkydrov@gmail.com>
2024-08-09 13:27:07 +0300

    Добавил задачу 5

---
+++ model/product.go
@@ -0,0 +1,13 @@
+package model
+
+//Создайте структуру Product с полями Name и Stock.
+//Напишите метод Sell с получателем по указателю, который уменьшает запас на переданное количество единиц.
+
+type Product struct {
+	Name  string
+	Stock int
+}
+
+func (p *Product) Sell(stock int) {
+	p.Stock -= stock
+}
---
+++ task3/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"taskReceiver1/model"
+)
+
+//Задача 5: Уменьшение запаса товара
+//Создайте структуру Product с полями Name и Stock.
+//Напишите метод Sell с получателем по указателю, который уменьшает запас на переданное количество единиц.
+
+func main() {
+	product := model.Product{Name: "TTT", Stock: 100}
+
+	product.Sell(50)
+
+	fmt.Printf("Product: %+v\n", product)
+}
